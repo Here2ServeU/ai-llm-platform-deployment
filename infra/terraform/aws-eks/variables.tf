46a1dc7c68e0c8d7e130aa99029808e17110f037
Here2ServerU <revnaweji@gmail.com>
2025-11-05 07:12:02 -0600

    Added more scripts

--- infra/terraform/aws-eks/variables.tf
+++ infra/terraform/aws-eks/variables.tf
@@ -1,9 +1,56 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
 
-variable "region" { type = string, default = "us-east-1" }
-variable "name"   { type = string, default = "llm-eks" }
-variable "kubernetes_version" { type = string, default = "1.29" }
-variable "vpc_cidr" { type = string, default = "10.60.0.0/16" }
-variable "azs" { type = list(string), default = ["us-east-1a","us-east-1b"] }
-variable "public_subnets" { type = list(string), default = ["10.60.0.0/20","10.60.16.0/20"] }
-variable "private_subnets" { type = list(string), default = ["10.60.32.0/20","10.60.48.0/20"] }
-variable "tags" { type = map(string), default = { Project = "ai-llm", Env = "dev" } }
+variable "name" {
+  description = "Cluster/base name"
+  type        = string
+  default     = "t2s-eks"
+}
+
+variable "kubernetes_version" {
+  description = "EKS version"
+  type        = string
+  default     = "1.29"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "164.45.0.0/16"
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_subnets" {
+  description = "Public subnets"
+  type        = list(string)
+  default     = ["164.45.0.0/20", "164.45.16.0/20"]
+}
+
+variable "private_subnets" {
+  description = "Private subnets"
+  type        = list(string)
+  default     = ["164.45.32.0/20", "164.45.48.0/20"]
+}
+
+variable "enable_ingress" {
+  description = "Install ingress-nginx"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default     = {
+    Project = "express-t2s"
+    Env     = "dev"
+  }
+}
